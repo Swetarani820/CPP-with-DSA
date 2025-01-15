450fbe05e1d410dc4eb31586f5aea643d90dfcda
Swetarani820 <153173168+Swetarani820@users.noreply.github.com>
2025-01-15 10:49:48 +0000

    create a new file

---
+++ Day1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+// C++ Run Commend :- g++ -std=c++17 -o 
+
+// C Run Commend   :- gcc -std=c17 -o
+
+int main()  {
+    cout<<"Hello Sweta Jee"<<endl;
+    return 0;
+}
